2b967633db35d864798a9a235dffc2eaae44a6e2
progjoe <progjoe@gmail.com>
2016-12-30 08:20:29 -0800

    Added - ConvertToWords

--- console_algorithm/console_algorithm/Program.cs
+++ console_algorithm/console_algorithm/Program.cs
@@ -10,6 +10,55 @@ namespace console_algorithm
     {
         static void Main(string[] args)
         {
+            int currency = 123;
+            Console.WriteLine(ConvertToWords(currency).ToString());
+            Console.ReadLine();
+        }
+
+        public static string ConvertToWords(int number)
+        {
+            string words = "";
+
+            if (number == 0) return "zero";
+
+            if (number < 0) return " minus " + ConvertToWords(Math.Abs(number));
+
+            if ((number / 1000000) > 0)
+            {
+                words += ConvertToWords(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += ConvertToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += ConvertToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "") words += " and ";
+
+                var UnitMaps = new[] {"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen",
+                                      "fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+                var TenMaps = new[] { "zero", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20) words += UnitMaps[number];
+                else
+                {
+                    words += TenMaps[number / 10];
+                    if ((number % 10) > 0) words += "-" + UnitMaps[number % 10];
+                }
+            }
+
+
+            return words;
         }
     }
 }
